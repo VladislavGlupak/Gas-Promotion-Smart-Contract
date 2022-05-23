e526b8e28924939d98946f4d170675949012524c
Vladislav Glupak <vglupak@gmail.com>
2022-05-23 13:26:44 -0700

    update contract

--- contracts/GasPromo.sol
+++ contracts/GasPromo.sol
@@ -2,9 +2,9 @@
 
 pragma solidity ^0.6.6;
 
-import "OpenZeppelin/openzeppelin-contracts@3.0.0/contracts/math/SafeMath.sol";
+import "OpenZeppelin/openzeppelin-contracts@3.0.0//contracts/token/ERC721/ERC721.sol";
 
-contract GasPromo {
+contract GasPromo is ERC721 {
 
     using SafeMath for uint;
 
@@ -13,6 +13,13 @@ contract GasPromo {
     bool public stopPromo;
     string public fullTokenName = "Gas Promo Token";
     string public gasToken = "GPT";
+    bool public isJoined = false;
+    uint256 public nftPrice;
+
+    struct gasNft {
+        address ownerNft;
+        uint256 nftRate;
+    }
 
     enum GasTypeRate { 
         REGULAR, 
@@ -22,11 +29,13 @@ contract GasPromo {
     address[] public clients;
     mapping(address => uint) public balances;
     mapping(GasTypeRate => uint256) public rates;
+    mapping(uint256 => gasNft) public nftCollection;
 
-    constructor() public {
+    constructor() ERC721(fullTokenName, gasToken) public {
         minGallons = 5;
-        balances[owner] = 1;
+        balances[owner] = 10000000;
         stopPromo = false;
+        nftPrice = 500;
         rates[GasTypeRate.REGULAR] = 1;
         rates[GasTypeRate.MIDDLE] = 2;
         rates[GasTypeRate.PREMIUM] = 3;
@@ -41,6 +50,19 @@ contract GasPromo {
         require(stopPromo == false, "Promotion is stopped.");
         _;
     }
+
+    modifier joinedPromo() {
+        for (uint256 i = 0; i < clients.length; i++){
+                if (msg.sender == clients[i]) {
+                    isJoined = true;
+                }
+        }
+        require(isJoined == true, "Please, join the promo!");
+        _;
+    }
+
+    event boughtGas(address buyer, uint256 amount);
+    event genNft(uint256 tokenId, uint256 amount);
 
     function enterPromo() public stopPromoFalse {
         clients.push(msg.sender);
@@ -60,7 +82,7 @@ contract GasPromo {
         return rates[gas];
     }
 
-    function buyGas(uint256 numberGallons, GasTypeRate gas) public payable stopPromoFalse {
+    function buyGas(uint256 numberGallons, GasTypeRate gas) public payable stopPromoFalse joinedPromo {
 
         uint256 amount = numberGallons * rates[gas];
 
@@ -68,7 +90,11 @@ contract GasPromo {
         require(amount <= balances[owner], "Sorry! Tokens are out!");
 
         balances[msg.sender] = balances[msg.sender].add(amount);
-        balances[owner] = balances[owner].sub(amount);        
+        balances[owner] = balances[owner].sub(amount); 
+
+        isJoined = false;  
+
+        emit boughtGas(msg.sender, amount);     
     }
 
     function getBalance() public view returns (uint) {
@@ -79,15 +105,37 @@ contract GasPromo {
         balances[owner] += value;
     }
 
-    function stopPromotion(bool _stopPromo) public returns (bool){
+    function stopPromotion(bool _stopPromo) public onlyOwner returns (bool){
         stopPromo = _stopPromo;
 
-        for (uint256 i=0; i < clients.length; i++){
+        for (uint256 i = 0; i < clients.length; i++){
             address client = clients[i];
             balances[client] = 0;
         }
         clients = new address[](0);
 
         return stopPromo;
+    }
+
+    function mintNft(string memory tokenURI) public returns (uint256) {
+        require(balances[msg.sender] >= nftPrice, "You balance is not enougth for generating NFT!");
+
+        uint256 tokenId = totalSupply();
+        address ownerNft = msg.sender;
+
+        balances[ownerNft] = balances[ownerNft].sub(nftPrice);
+
+        _mint(ownerNft, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+
+        nftCollection[tokenId] = gasNft(ownerNft, nftPrice);
+
+        emit genNft(tokenId, nftPrice);
+
+        return tokenId;
+    }
+
+    function setNftPrice(uint256 _newNftPrice) public {
+        nftPrice = _newNftPrice;
     }
 }
